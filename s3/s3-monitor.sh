fb49144e2fcae61a0c7b6a5305ee9ebcedb0ea71
jiaozhenqing <jiaozhenqing@jd.com>
2020-01-17 11:27:50 +0800

    udpate

---
+++ s3/s3-monitor.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#set -x
+#set encoding=utf-8
+
+#定义的是云存储文件的内容
+readonly VALUE="success"
+
+#定义的是命令执行的超时时间
+readonly TIMESEC="3"
+readonly URL="http://monitor.s3-internal.cn-east-2.jdcloud-oss.com/s3-monitor.txt"
+#将输出结果默认赋值
+result="-1"
+s3_monitor_status="-1"
+
+#检查输出文件的目录，文件和权限
+function check_tools
+{
+    mkdir -p  /var/lib/node_exporter/textfile
+    cd /var/lib/node_exporter/textfile && touch s3_monitor.prom && chmod 755 s3_monitor.prom
+}
+
+#通过云存储的接口获取指定文件的内容并和预先定义的内容进行比对
+function check_result
+{
+    result=$( timeout $TIMESEC curl -s $URL)
+    cd /var/lib/node_exporter/textfile
+
+    if [ "$result" == "$VALUE" ];then
+        echo "s3_monitor_status 0" > s3_monitor.prom
+    else
+        echo "s3_monitor_status 1" > s3_monitor.prom
+    fi
+}
+
+function main
+{
+    check_tools
+    check_result
+}
+
+main

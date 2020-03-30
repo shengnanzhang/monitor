afbdcf24cf919fc433ac5c0a40cc7c27dbc9511b
cloud-op <jiaozhenqing@gmail.com>
2020-03-30 16:04:39 +0800

    Update dns-monitor-plus.sh

--- dns/dns-monitor-plus.sh
+++ dns/dns-monitor-plus.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#set -x
-#set encoding=utf-8
-#source ~/.bash_profile
 readonly DOMAIN="monitor.site7x24.net.cn"
 
 #定义的是命令执行的超时时间
@@ -11,7 +8,7 @@ readonly COMMAND="dig"
 
 HOSTVALUE=$(date +%M|sed -r 's/0*([0-9])/\1/')
 
-NS=$1
+Domainlist=(ns1.jdgslb.com ns2.jdgslb.com ns3.jdgslb.com ns4.jdgslb.com ns5.jdgslb.com ns6.jdgslb.com vip1.jdgslb.com vip2.jdgslb.com 8.8.8.8)
 
 #将输出结果默认赋值
 result="-1"
@@ -35,37 +32,34 @@ function check_prometheus
 #对获取的value和预先定义好的value进行对比，判断结果是否正常
 function check_result
 {
-    #判断输入的参数数量是否为1，如果为1，则将NS设置为$1；如果不为1，直接上默认的8.8.8.8
-    if [ $# -eq 1 ];then
-        NS=$1
-    else
-        NS="8.8.8.8"
-    fi
+    for NS in ${Domainlist[@]};
+	    do
+            echo $NS
+	    #直接将dig的结果获取的IP的最后一位拿出来
+	    result=$(timeout $TIMESEC $COMMAND $DOMAIN @"$NS" +short|head -n 1|cut -d "." -f4)
 
-    #直接将dig的结果获取的IP的最后一位拿出来
-    result=$(timeout $TIMESEC $COMMAND $DOMAIN @"$NS" +short|head -n 1|cut -d "." -f4)
+	    #如果result有结果且结果是数字的话，则进行下面的处理；避免result没有获取到结果就参与计算，导致误报
+	    if [ "$result" -ge 1 ];then
+	    if [ "$HOSTVALUE" -ge "$result" ];then
+		cost=$((HOSTVALUE - result ))
+	    else
+		#对于跨周期的情况，实际时间会小于dns的结果，因此需要加一个60的周期进行补偿
+		cost=$((HOSTVALUE + 60 - result ))
+	    fi
 
-    #如果result有结果且结果是数字的话，则进行下面的处理；避免result没有获取到结果就参与计算，导致误报
-    if [ "$result" -ge 1 ];then
-	if [ "$HOSTVALUE" -ge "$result" ];then
-	    cost=$((HOSTVALUE - result ))
-	else
-	    #对于跨周期的情况，实际时间会小于dns的结果，因此需要加一个60的周期进行补偿
-	    cost=$((HOSTVALUE + 60 - result ))
-	fi
+	    cd /var/lib/node_exporter/textfile && echo -e "dnsplus_monitor_status{target=\"$NS\"} 0\ndnsplus_monitor_cost{target=\"$NS\"} $cost" >>  dnsplus_monitor.prom
+	    else
+	    cd /var/lib/node_exporter/textfile && echo -e "dnsplus_monitor_status{target=\"$NS\"} -1\ndnsplus_monitor_cost{target=\"$NS\"} -1" >>  dnsplus_monitor.prom
 
-	cd /var/lib/node_exporter/textfile && echo -e "dnsplus_monitor_status{target=\"$NS\"} 0\ndnsplus_monitor_cost{target=\"$NS\"} $cost" >>  dnsplus_monitor.prom
-    else
-	cd /var/lib/node_exporter/textfile && echo -e "dnsplus_monitor_status{target=\"$NS\"} -1\ndnsplus_monitor_cost{target=\"$NS\"} -1" >>  dnsplus_monitor.prom
-
-    fi
+	    fi
+    done
 }
 
 function main
 {
     check_tools
     check_prometheus
-    check_result  "$@"
+    check_result  
 }
 
-main "$@"
+main 

3c43a5f61893b0e981245790833a8127a425bc24
cloud-op <jiaozhenqing@gmail.com>
2020-04-01 12:03:43 +0800

    Update nat-monitor.sh

--- nat/nat-monitor.sh
+++ nat/nat-monitor.sh
@@ -40,7 +40,8 @@ function check_result
     done
 
     length=${#Domainlist[@]}
-    if [ "$length" -ge "$((result / 2))" ];then
+    
+    if [ "$result" -ge "$((length / 2))" ];then
         cd /var/lib/node_exporter/textfile && echo "nat_monitor_status 0" >  nat_monitor.prom
     else
         cd /var/lib/node_exporter/textfile && echo "nat_monitor_status 1" >  nat_monitor.prom
